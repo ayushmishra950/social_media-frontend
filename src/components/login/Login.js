744ad51673af616373614374085c1f61706fb708
ayushmishra950 <ayush950mishra@gmail.com>
2025-06-20 17:57:45 +0530

    Update Login.js

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -79,7 +79,7 @@ const Login = () => {
           />
         </div>
 
-        <div className="mb-6">
+              <div className="mb-6">
           <label className="block text-gray-700 mb-1" htmlFor="password">
             Password
           </label>
@@ -89,6 +89,9 @@ const Login = () => {
             value={formData.password}
             onChange={handleChange}
             required
+            minLength={8}
+            pattern="^(?=.*[A-Za-z])(?=.*\d)(?=.*[@]).{8,}$"
+            title="Password must be at least 8 characters long and include letters, digits, and the @ symbol."
             className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
             placeholder="Enter your password"
           />
